9dd9e334636ec8123b2bf4378255e81234ce099d
u-know-who <u-know-who@users.noreply.github.com>
2016-04-03 18:10:33 +0530

    Create reqno.php

---
+++ reqno.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+include_once 'dbconnect.php';
+if(!isset($_SESSION['user'])!="")
+{
+    header("Location: login.php");
+}
+$accept_id = $_GET['accept_id'];
+$sqlr1  = mysql_query("UPDATE users SET  proxy_points=proxy_points + 10 WHERE user_id = ".$accept_id);
+
+?>
+
